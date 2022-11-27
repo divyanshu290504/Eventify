cbd3e62a312458e542ed67ce24964f857e0b2936
Divyanshu Kumar <85216941+divyanshu290504@users.noreply.github.com>
2022-11-27 09:44:48 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import NavBar from '../components/NavBar';
+import TabsBar from '../components/TabsBar';
+import Body from '../components/Body'
+export default function Home() {
+    const div_style={
+            height:'auto',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundImage: "url('https://img.freepik.com/premium-vector/abstract-blue-purple-gradient-color-background-website-banner-graphic-design_120819-729.jpg')"
+        };
+    return (
+        <div style={div_style}>
+            <NavBar />
+            <TabsBar />
+            <Body />
+        </div>
+    );
+}
